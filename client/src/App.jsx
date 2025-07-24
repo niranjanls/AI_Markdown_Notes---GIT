88863f5ea516b0afd4218b30d4b873f7d06c8c19
niranjanls <lsniranjan2005@gmail.com>
2025-07-24 13:58:12 +0530

    Fix: corrected case-sensitive import for MarkdownEditor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MarkdownEditor from './components/MarkDownEditor'
+import MarkdownEditor from './components/MarkdownEditor'
 
 function App() {
   return (
